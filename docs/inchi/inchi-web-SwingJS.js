f35175e314d695ba48459935c434fdf8d8652e56
bobhanson <hansonr@stolaf.edu>
2025-01-31 14:05:10 -0600

    minor adjustments in inchi-web-SwingJS.js

--- docs/inchi/inchi-web-SwingJS.js
+++ docs/inchi/inchi-web-SwingJS.js
@@ -4854,23 +4854,33 @@ else if (typeof exports === 'object')
 
 "use strict";
 
+var isSwingJS = !!self.J2S;
+
+self.Jmol || (self.Jmol = {});
+self.J2S || (self.J2S = self.Jmol);
+
+
 var module;
 /*
  * singleton asynchronous WASM module(s) initialization required 
  * BEFORE processing any query.
  * 
- * In Jmol we will need to do this before the actual command script is executed.
+ * In J2S we will need to do this before the actual command script is executed.
  *
  * Calling the factory function returns a Promise which resolves to the module object.
  * See https://github.com/emscripten-core/emscripten/blob/fa339b76424ca9fbe5cf15faea0295d2ac8d58cc/src/settings.js#L1183
  */
- (async function getInchiModule() {
-	 module = await inchiModule();
- })();
- 
-
- self.Jmol || (self.Jmol = {});
- self.J2S || (self.J2S = self.Jmol);
+(async function getInchiModule() {
+   if (isSwingJS) {
+	 module = await inchiModule({
+		 "locateFile":function(f) {
+			 return J2S.inchiPath + "/" + f;
+		 }
+	 });
+   } else {
+		 module = await inchiModule();	   
+   }
+})();
  
 /*
  * Glue code to invoke the C functions in inchi_web.c
@@ -4880,48 +4890,48 @@ var module;
  * See https://github.com/emscripten-core/emscripten/issues/6484 (Emscripten does
  * not do this on its own when using "string" as return type.)
  */
-Jmol.inchiFromMolfile = function(molfile, options) {
+J2S.inchiFromMolfile = function(molfile, options) {
   const ptr = module.ccall("inchi_from_molfile", "number", ["string", "string"], [molfile, options]);
   const result = module.UTF8ToString(ptr);
   module._free(ptr);
   return JSON.parse(result);
 }
 
-Jmol.inchikeyFromInchi = function(inchi) {
-  const ptr = module.ccall("inchikey_from_inchi", "number", ["string"], [inchi]);
-  const result = module.UTF8ToString(ptr);
-  module._free(ptr)
-  return JSON.parse(result);
-}
-
-Jmol.molfileFromInchi = function(inchi, options) {
-  const ptr = module.ccall("molfile_from_inchi", "number", ["string", "string"], [inchi, options]);
-  const result = module.UTF8ToString(ptr);
-  module._free(ptr);
-  return JSON.parse(result);
-}
-
-Jmol.molfileFromAuxinfo = function(auxinfo, bDoNotAddH, bDiffUnkUndfStereo) {
-  const ptr = module.ccall("molfile_from_auxinfo", "number", ["string", "number", "number"], [auxinfo, bDoNotAddH, bDiffUnkUndfStereo]);
-  const result = module.UTF8ToString(ptr);
-  module._free(ptr);
-  return JSON.parse(result);
-}
-
-Jmol.modelFromInchi = function(inchi, options) {
-  const ptr = module.ccall("model_from_inchi", "number", ["string", "string"], [inchi, options]);
-  const result = module.UTF8ToString(ptr);
-  module._free(ptr);
-  return JSON.parse(result);
-}
-
-Jmol.inchiFromInchi = function(inchi, options) {
+J2S.inchiFromInchi = function(inchi, options) {
   const ptr = module.ccall("inchi_from_inchi", "number", ["string", "string"], [inchi, options]);
   const result = module.UTF8ToString(ptr);
   module._free(ptr)
   return JSON.parse(result);
 }
 
+J2S.inchikeyFromInchi = function(inchi) {
+  const ptr = module.ccall("inchikey_from_inchi", "number", ["string"], [inchi]);
+  const result = module.UTF8ToString(ptr);
+  module._free(ptr)
+  return JSON.parse(result);
+}
 
+J2S.modelFromInchi = function(inchi, options) {
+  const ptr = module.ccall("model_from_inchi", "number", ["string", "string"], [inchi, options]);
+  const result = module.UTF8ToString(ptr);
+  module._free(ptr);
+  return JSON.parse(result);
+}
+
+J2S.molfileFromInchi = function(inchi, options) {
+  const ptr = module.ccall("molfile_from_inchi", "number", ["string", "string"], [inchi, options]);
+  const result = module.UTF8ToString(ptr);
+  module._free(ptr);
+  return JSON.parse(result);
+}
+
+J2S.molfileFromAuxinfo = function(auxinfo, bDoNotAddH, bDiffUnkUndfStereo) {
+  const ptr = module.ccall("molfile_from_auxinfo", "number", ["string", "number", "number"], [auxinfo, bDoNotAddH, bDiffUnkUndfStereo]);
+  const result = module.UTF8ToString(ptr);
+  module._free(ptr);
+  return JSON.parse(result);
+}
+
+console.log("inchi-web-SwingJS loaded");	
 
 
